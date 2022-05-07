60fbaacb0b5eb220834231af8c940c760069baeb
SunLxy <1011771396@qq.com>
2022-05-07 14:36:41 +0800

    fix:优化代码

--- core/src/utils/transform.ts
+++ core/src/utils/transform.ts
@@ -11,10 +11,9 @@ export const getTransformValue = (str: string, filename: string, funName: string
   const isReact = /import React.+from ("|')react("|')/.test(str);
   // 先判断 是否引入 react
   const tran = isReact ? str : `import React from "react"\n ${str}`;
-  const code = `${babelTransform(tran, `${filename}`).code}`
-    .replace(`Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});`, '')
-    .replace(`exports["default"] = void 0;`, '')
-    .replace(`exports["default"] = _default;`, `return _react["default"].createElement(_default)`);
+  /** 先把默认导出 export default 进行替换 **/
+  const newCode = `${tran.replace('export default', 'const _default = ')}\n`;
+  const code = `${babelTransform(newCode, `${filename}`).code}\n return _react["default"].createElement(_default)`;
 
   return `function ${funName}(){
     ${code}
