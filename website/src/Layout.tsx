74b5f74381d849c7ec2008e56bf2f31581d22d00
jaywcjlove <398188662@qq.com>
2022-05-07 18:59:14 +0800

    website: add github corners.

--- website/src/Layout.tsx
+++ website/src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
+import GitHubCorners from '@uiw/react-github-corners';
 import '@wcj/dark-mode';
 import styles from './Layout.module.less';
 
 export function Layout() {
   return (
     <Fragment>
+      <GitHubCorners target="__blank" fixed href="https://github.com/kktjs/markdown-react-code-preview-loader" />
       <header className={styles.header}>
         <nav>
           <NavLink to="/" replace>
