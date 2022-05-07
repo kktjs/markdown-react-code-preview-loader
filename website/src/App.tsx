c47dfa0f30313e3ee48dbd6328299b1a1bbde1d1
jaywcjlove <398188662@qq.com>
2022-05-07 19:05:26 +0800

    website: modify hash router.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
 import { HomePage } from './pages/docs';
 import { ExamplePage } from './pages/example';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/example" element={<ExamplePage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
