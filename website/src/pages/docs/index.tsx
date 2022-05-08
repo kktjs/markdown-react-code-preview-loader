3834a006ce1a1b3476d6867f7cd58482b24fe488
SunLxy <1011771396@qq.com>
2022-05-08 18:48:44 +0800

    style:修改鼠标移入标题样式

--- website/src/pages/docs/index.tsx
+++ website/src/pages/docs/index.tsx
@@ -1,13 +1,16 @@
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import useMdData from './../../components/useMdData';
 import { Loader } from 'uiw';
+import styles from './../index.module.less';
 export function HomePage() {
   const { mdData, loading } = useMdData((path) => {
     return import(`markdown-react-code-preview-loader/README${path}.md`);
   });
   return (
     <Loader style={{ width: '100%' }} loading={loading} tip="loading...">
-      <MarkdownPreview source={mdData.source} />
+      <div>
+        <MarkdownPreview className={styles.markdown} source={mdData.source} />
+      </div>
     </Loader>
   );
 }
--- website/src/pages/example/index.tsx
+++ website/src/pages/example/index.tsx
@@ -2,7 +2,7 @@ import MarkdownPreview from '@uiw/react-markdown-preview';
 import PreView from '../../components/CodeLayout';
 import useMdData from './../../components/useMdData';
 import { Loader } from 'uiw';
-
+import styles from './../index.module.less';
 const getMetaData = (meta: string) => {
   if (meta) {
     const [metaItem] = /mdx:(.[\w|:]+)/i.exec(meta) || [];
@@ -19,39 +19,42 @@ export function ExamplePage() {
   const { mdData, loading } = useMdData((path) => import(`./App${path}.md`));
   return (
     <Loader style={{ width: '100%' }} loading={loading} tip="loading...">
-      <MarkdownPreview
-        source={mdData.source}
-        components={{
-          code: ({ inline, node, ...props }) => {
-            const {
-              'data-meta': meta,
-              noPreview,
-              noScroll,
-              bgWhite,
-              noCode,
-              codePen,
-              codeSandboxOption,
-              codeSandbox,
-              ...rest
-            } = props as any;
-            if (inline) {
-              return <code {...props} />;
-            }
-            const line = node.position?.start.line;
-            const funName = getMetaData(meta || '') || line;
-            const Child = mdData.components[funName || ''];
-            if (funName && typeof Child === 'function') {
-              const copyNodes = mdData.codeBlock[funName] || '';
-              return (
-                <PreView code={<code {...rest} />} copyNodes={copyNodes}>
-                  <Child />
-                </PreView>
-              );
-            }
-            return <code {...rest} />;
-          },
-        }}
-      />
+      <div>
+        <MarkdownPreview
+          source={mdData.source}
+          className={styles.markdown}
+          components={{
+            code: ({ inline, node, ...props }) => {
+              const {
+                'data-meta': meta,
+                noPreview,
+                noScroll,
+                bgWhite,
+                noCode,
+                codePen,
+                codeSandboxOption,
+                codeSandbox,
+                ...rest
+              } = props as any;
+              if (inline) {
+                return <code {...props} />;
+              }
+              const line = node.position?.start.line;
+              const funName = getMetaData(meta || '') || line;
+              const Child = mdData.components[funName || ''];
+              if (funName && typeof Child === 'function') {
+                const copyNodes = mdData.codeBlock[funName] || '';
+                return (
+                  <PreView code={<code {...rest} />} copyNodes={copyNodes}>
+                    <Child />
+                  </PreView>
+                );
+              }
+              return <code {...rest} />;
+            },
+          }}
+        />
+      </div>
     </Loader>
   );
 }
